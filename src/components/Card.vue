ea4cb4bedb593c393632fdb8cc72e65be708dfab
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 15:08:07 +0330

    Card component added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,52 @@
-<template>
-    <div>
-
-    </div>
-</template>
-
 <script setup>
-
+const { name, image, species } = defineProps([
+    'name',
+    'image',
+    'species',
+])
 </script>
 
-<style lang="scss" scoped>
+<template>
+        <NCard class="n-card">
+            <template #cover>
+                <img :src="image">
+            </template>
+            <div class="content">
+                <h1>{{ name }}</h1>
+                <p>{{ species }}</p>
+            </div>
+        </NCard>
+</template>
+
+
+
+<style scoped>
+.n-card {
+    height: 400px;
+    width: 300px;
+}
+.n-card:hover {
+    transform: scale(0.9);
+    transition: transform 0.5s ease;
+}
+
+.n-card img {
+    object-fit: cover;
+    height: 280px;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.content h1 {
+    font-size: 20px;
+}
+
+.content p {
+    font-size: 16px;
+}
 
 </style>
