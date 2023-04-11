af30483dc22893a82b8ddd4f7fec996e48f00448
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 19:28:48 +0330

    Added error component

---
+++ src/components/ErrorCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { hasError } = defineProps(['hasError'])
+</script>
+
+<template>
+    <div>
+        <div class="error-card">
+            <NCard title="Error">
+                <div class="error-message">{{ hasError }}</div>
+            </NCard>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.error-card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 50px auto;
+    width: 600px;
+}
+
+.error-message {
+    font-size: 20px;
+    color: red;
+    margin: 20px 0;
+}
+</style>
