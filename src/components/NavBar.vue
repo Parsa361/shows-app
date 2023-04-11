c244220f19485137ac4b9d74a3ec7ec8a865740d
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 19:29:33 +0330

    made it responsive and emit the input value

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,35 @@
 <script setup>
+import { defineEmits } from 'vue'
 
+const emit = defineEmits(['update-search'])
+
+const updateSearch = (event) => {
+    emit('update-search', event.target.value)
+}
 </script>
 
 <template>
     <header>
         <div class="search">
-            <input type="text" placeholder="search cards...">
+            <input @input="updateSearch" type="text" placeholder="search characters...">
         </div>
     </header>
 </template>
-
 
 
 <style scoped>
 header {
     background: no-repeat center url('../../rick-and-morty-netflix-2020-9d-2560x1440.jpg');
     background-size: cover;
-    height: 30rem;
+    height: 40rem;
     display: flex;
     justify-content: center;
 }
 
 .search input {
+    box-sizing: border-box;
     padding: 10px 20px;
-    min-width: 300px;
+    min-width: 250px;
     border-radius: 10px;
     margin: 10px;
     outline: none;
