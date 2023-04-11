a63e6c0d2d8806a76559d718765226f6d048900a
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 19:31:18 +0330

    Added the buttons for pagination in the component

---
+++ src/components/paginationBtn.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['increment', 'decrement'])
+const { isLoading } = defineProps(['isLoading'])
+
+const page = ref(1)
+
+const increment = () => {
+    page.value++
+    emit('increment', page.value)
+}
+const decrement = () => {
+    page.value--
+    emit('decrement', page.value)
+}
+</script>
+
+<template>
+    <div class="btn-container">
+        <button @click="decrement" :disabled="page <= 1">&lt</button>
+        <button @click="increment" :disabled="isLoading">></button>
+    </div>
+</template>
+
+<style scoped>
+.btn-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+
+button {
+    background-color: #FAEBD7;
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    cursor: pointer;
+    border: 2px solid rgb(233, 138, 5);
+    margin: 20px 0;
+}
+</style>
