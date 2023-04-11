52032a207b97a6b5ef6f3990d21c9fc5aad1a38e
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 14:44:01 +0330

    The navbar,home and also loading spinner are added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
-
+import NavBar from './components/NavBar.vue';
+import RickMortyCards from './views/RickMortyCards.vue';
 </script>
 
 <template>
-  <RouterView />
+  <NavBar />
+
+  <Suspense>
+    <template #default>
+      <RickMortyCards />
+    </template>
+    <template #fallback>
+      <NSpace class="spinner">
+        <n-spin size="large" stroke="green" />
+      </NSpace>
+    </template>
+  </Suspense>
 </template>
 
 <style scoped>
-
+.spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
