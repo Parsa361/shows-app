226964eddc7b10e57d229a6bebca2fa892c13fca
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-11 19:26:58 +0330

    Delete Suspense and handle loading in onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <script setup>
-import NavBar from './components/NavBar.vue';
 import RickMortyCards from './views/RickMortyCards.vue';
+
 </script>
 
 <template>
-  <NavBar />
-
-  <Suspense>
-    <template #default>
-      <RickMortyCards />
-    </template>
-    <template #fallback>
-      <NSpace class="spinner">
-        <n-spin size="large" stroke="green" />
-      </NSpace>
-    </template>
-  </Suspense>
+  <RickMortyCards />
 </template>
 
-<style scoped>
-.spinner {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+<style scoped></style>
