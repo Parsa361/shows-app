62fbb04d32eaf5241a7759e2ec3053ce1251671e
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-12 19:30:32 +0330

    devide contents into the components and importthem

--- src/views/RickMortyCards.vue
+++ src/views/RickMortyCards.vue
@@ -1,23 +1,24 @@
 <script setup>
-import '../assets/animation.css'
 import axios from 'axios'
 import Card from '../components/Card.vue'
 import NavBar from '../components/NavBar.vue';
 import ErrorCard from '../components/ErrorCard.vue'
 import defineErrors from '../composable/defineErrors';
-import { onMounted, ref, watch } from 'vue'
 import PaginationBtn from '../components/paginationBtn.vue';
+import Loading from '../components/Loading.vue';
+import { onMounted, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 const { hasError, handleError } = defineErrors()
 const characters = ref(null)
 const isLoading = ref(false)
 const page = ref(0)
 const search = ref('')
+const router = useRouter()
 
 const updateSearch = (searchValue) => {
     search.value = searchValue
 }
-
 const increment = (paginationPage) => {
     page.value = paginationPage
 }
@@ -63,26 +64,33 @@ watch(search, async () => {
     <NavBar @update-search="updateSearch" />
 
     <div class="container">
-        <PaginationBtn @increment="increment" @decrement="decrement" :isLoading="isLoading" />
+        <PaginationBtn 
+            @increment="increment" 
+            @decrement="decrement" 
+            :isLoading="isLoading" />
 
         <div v-if="isLoading && !hasError">
-            <div class="spin-container">
-                <NSpace>
-                    <NSpin size="large" class="spiner" />
-                </NSpace>
-            </div>
+            <Loading />
         </div>
 
-        <div v-if="!hasError && !isLoading" class="cards">
+        <div v-if="characters" class="cards">
             <TransitionGroup name="fade">
-                <Card v-for="character in characters" :key="character.id" :name="character.name" :image="character.image"
-                    :species="character.species" />
+                <Card 
+                    @click="router.push(`/rick-morty/${character.id}`)" 
+                    v-for="character in characters"
+                        :key="character.id" 
+                        :name="character.name" 
+                        :image="character.image" 
+                        :species="character.species" />
             </TransitionGroup>
         </div>
 
         <ErrorCard v-if="hasError" :hasError="hasError" />
 
-        <PaginationBtn @increment="increment" @decrement="decrement" :isLoading="isLoading" />
+        <PaginationBtn 
+            @increment="increment" 
+            @decrement="decrement" 
+            :isLoading="isLoading" />
     </div>
 </template>
 
@@ -101,14 +109,19 @@ watch(search, async () => {
     gap: 30px 12px;
 }
 
-.spin-container {
-    position: relative;
-    margin: 20px auto;
+.fade-enter-from {
+    opacity: 0;
+    transform: scale(0);
+
 }
 
-.spiner {
-    position: absolute;
-    right: 50%;
-    left: 50%;
+.fade-enter-to {
+    opacity: 1;
+    transform: scale(1);
+
+}
+
+.fade-enter-active {
+    transition: all 1s ease;
 }
 </style>
