7a1b3b21812cba669601b4ee4ac448524df8a6ae
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-12 19:27:32 +0330

    Add some css reset in it

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,29 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  background-color: black;
+}
 
+/* HTML5 display-role reset for older browsers */
+figure,
+footer,
+header,
+menu,
+nav,
+section {
+  display: block;
+}
+
+body {
+  line-height: 1;
+}
 </style>
