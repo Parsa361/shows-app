f0d76e1f482155596e1bcf96edbbe84321ee3265
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-12 19:30:04 +0330

    Add single character dynamic route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RickMortyCards from '../views/RickMortyCards.vue'
+import RickMortyCard from '../views/RickMortyCard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: RickMortyCards
     },
+    {
+      path: '/rick-morty/:id',
+      name: 'rick-morty',
+      component: RickMortyCard
+    }
   ]
 })
 
